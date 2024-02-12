a5b6e5aa1054390dd59dc9017aa0726c50c69bca
Vikram0817 <vikramchaudhary8700@gmail.com>
2024-02-12 10:39:43 +0530

    make signout

--- frontend/src/components/UserDropdown.tsx
+++ frontend/src/components/UserDropdown.tsx
@@ -1,6 +1,14 @@
 import { useNavigate } from "react-router-dom"
+import { useSetRecoilState } from "recoil";
+import { firstNameState, lastNameState, passwordState, usernameState } from "../store";
 
 export default function UserDropdown(){
+
+    const setUserName = useSetRecoilState(usernameState);
+    const setPassword = useSetRecoilState(passwordState);
+    const setFirstName = useSetRecoilState(firstNameState);
+    const setLastName = useSetRecoilState(lastNameState);
+    
 
     const navigate = useNavigate();
 
@@ -12,6 +20,15 @@ export default function UserDropdown(){
     function handleInvite() {
       navigator.clipboard.writeText("https://type-script-paytm.vercel.app/");
       alert("Link for Paytm-Typescript copied to clipboard")
+    }
+
+    function handleSignout() {
+      setUserName("");
+      setPassword("");
+      setFirstName("");
+      setLastName("");
+      localStorage.removeItem("myToken");
+      navigate("/");
     }
 
     return(
@@ -27,7 +44,7 @@ export default function UserDropdown(){
         <a onClick={handleInvite} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Invite Friends</a>
       </li>
       <li>
-        <a onClick={() => navigate("/")} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
+        <a onClick={handleSignout} className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
       </li>
     </ul>
 </div>
